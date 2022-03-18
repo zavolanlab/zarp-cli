c77f9066e0b3dae32833358aab72ebde242d395d
dominikburri <dominik.burri19@gmail.com>
2022-03-18 11:54:16 +0100

    add missing docstrings

--- zarp/run/snakemake.py
+++ zarp/run/snakemake.py
@@ -7,14 +7,40 @@ from zarp.config.models import Run
 
 
 class SnakemakeExecutor:
-    """Run snakemake with system calls."""
+    """Run snakemake with system calls.
+    
+    Args:
+        run (Run): Run-specific parameters.
+
+    Attributes:
+        run_dict (dict): Dictionary with run-specific parameters.
+        success (bool): Indicator for successful snakemake run.
+    
+    Example:
+        The example below expects a valid `Snakefile` in the current working
+        directory. It constructs a run with default values and runs it. 
+        >>> mysnk = SnakemakeExecutor(zarp.config.models.Run())
+        >>> mysnk.prepare_run(snkfile = "Snakefile", workdir = ".")
+        >>> mysnk.run()
+        >>> assert mysnk.get_success()
+
+    """
 
     def __init__(self, run: Run) -> None:
+        """Class constructor."""
         self.run_dict = run.dict()
         self.success = None
 
     def prepare_run(self, snkfile: str, workdir: str) -> None:
         """Configure list of strings for execution.
+
+        Args:
+            snkfile (str): Path to Snakefile.
+            workdir (str): Path to workdir.
+        
+        Raises:
+            KeyError: If `execution_profile` not in run_dict.
+
         """
         run_list = ["snakemake"]
         # Snakemake config
@@ -29,11 +55,23 @@ class SnakemakeExecutor:
 
     def validate_run(self) -> bool:
         """Ensure the list of strings is a correct Snakemake call.
+
+        Returns:
+            bool: True, if constructed run_list is a valid snakemake call, 
+                False otherwise.
+
         """
         pass
 
     def run(self) -> None:
-        """Execute Snakemake with system call."""
+        """Execute Snakemake with system call.
+        
+        Run Snakemake with a system call, errors there are not handed over.
+        
+        Raises:
+            CalledProcessError: by `subprocess.run()`
+
+        """
         try:
             completed = subprocess.run(self.run_list)
             if completed.returncode == 0:
@@ -46,4 +84,11 @@ class SnakemakeExecutor:
             print(e)
     
     def get_success(self) -> bool:
+        """Obtain whether run successful or not.
+
+        If not yet run, success == None.
+
+        Returns:
+            bool: True, if Snakemake exited with code 0, False otherwise.
+        """
         return self.success
